431758205138c0b25eaed38149efe828fcafa87a
damjan <magfixcorp@gmail.com>
2020-10-30 08:56:36 +0100

    build for dse + mjml fix

--- src/shared/services/mjmlService.ts
+++ src/shared/services/mjmlService.ts
@@ -15,11 +15,11 @@ export default class MjmlService {
   public renderTemplate (entity: any) {
     return new Promise((resolve, reject) => {
       const token ='1a336b03-7c52-49a5-9f5f-b669dbf13c06:dfd53576-0500-4694-904f-1b8b526ac1ee'
-      axios.post('https://api.mjml.io/v1', { mjml: entity }, {headers:{
+      axios.post('/api/mjml', { mjml: entity }, {headers:{
         'Authorization': 'Basic ' + token,
-          'origin': 'https://api.mjml.io/v1',
           'sec-fetch-mode': 'no-cors',
           'Sec-Fetch-Site': 'none',
+          'Access-Control-Allow-Credentials': 'true'
         }}).then(resp => {
         resolve(resp)
       }).catch(err => {
