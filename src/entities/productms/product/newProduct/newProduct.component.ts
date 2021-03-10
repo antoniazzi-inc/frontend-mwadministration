fce3a217f807eb1199d1e4a79578015a184f9000
damjan <magfixcorp@gmail.com>
2021-03-10 17:49:00 +1100

    adding licence header

--- src/entities/productms/product/newProduct/newProduct.component.ts
+++ src/entities/productms/product/newProduct/newProduct.component.ts
@@ -1,3 +1,22 @@
+/*
+ *
+ *  * Copyright 2018-2021 Antoniazzi Holding BV
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify it
+ *  * under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License,
+ *  * or (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 import {mixins} from 'vue-class-component'
 import CommonHelpers from '@/shared/commonHelpers'
 import {Component, Vue, Watch} from 'vue-property-decorator'
@@ -537,7 +556,8 @@ export default class NewProductComponent extends mixins(Vue, CommonHelpers) {
         userDefinedPrice: false,
         productSubscription: self.product.productSubscription,
         payButtonJson: self.product.payButtonJson,
-        priceRounding: self.product.priceRounding
+        priceRounding: self.product.priceRounding,
+        productPaymentMethods: self.$store.state.lookups.paymentMethods.map((method: any) => method.value)
       }
       switch (this.product.productType) {
         case 'DIGITAL':
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
+/*
+ *
+ *  * Copyright 2018-2021 Antoniazzi Holding BV
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify it
+ *  * under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License,
+ *  * or (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 import Vue from 'vue'
 import 'bootstrap'
 import VeeValidate from 'vee-validate'
@@ -10,10 +29,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import * as filters from './shared/filters'
-import * as enTranslations from '@/i18n/en'
 import * as deTranslations from '@/i18n/de'
 import * as esTranslations from '@/i18n/es'
 import * as frTranslations from '@/i18n/fr'
+import * as enTranslations from '@/i18n/en'
 import * as nlTranslations from '@/i18n/nl'
 import setupAxiosInterceptors from '@/shared/axios-interceptors'
 import 'vue-form-wizard/dist/vue-form-wizard.min.css'
@@ -27,8 +46,8 @@ Vue.use(VeeValidate, {
 })
 Vue.use(VueFormWizard)
 Vue.config.productionTip = false
-Vue.use(VueI18n)
 filters.initFilters()
+Vue.use(VueI18n)
 const messages = {
   en: enTranslations,
   de: deTranslations,
